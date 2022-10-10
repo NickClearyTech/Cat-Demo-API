90f50fe899edf5607bfeeca08fb67558daa0b9ab
Nick Cleary <nicholascleary01@gmail.com>
2022-10-10 19:26:04 -0400

    Pushing items

---
+++ app/demo/demoapp/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets, mixins
+from django_filters.rest_framework import DjangoFilterBackend
+from demoapp.models import Dog, Cat
+from demoapp.serializers import CatSerializer, DogSerializer
+from drf_spectacular.utils import extend_schema
+
+
+@extend_schema(tags=["Cat"])
+class CatViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin
+):
+    serializer_class = CatSerializer
+    queryset = Cat.objects.all()
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["name", "breed", "age", "sex", "adopted", "weight"]
+    
